2e21358a7a19c2f7827c291d723994e6eab32976
aysss <aysss007@users.noreply.github.com>
2020-12-13 10:19:12 +0800

    modify

--- Script/ay_sleepcycle.js
+++ Script/ay_sleepcycle.js
@@ -1,4 +1,53 @@
-let obj = JSON.parse($response.body);
-obj.subscription.package_id= "premium";
-$done({body: JSON.stringify(obj)});
+var obj = JSON.parse($response.body);
 
+obj = {
+  "user": {
+    "created_date": 1607746110,
+    "email": "arthury.me@gmail.com",
+    "display_name": null,
+    "uid": "VTo0ODAzNjMyMjAyMTIxMjE2",
+    "user_id": "ahFtZGxhYnMtc2xlZXBjeWNsZXIRCxIEVXNlchiAgKi4jJzECAw"
+  },
+  "subscription": {
+    "product_id": null,
+    "transaction_id": null,
+    "campaign": "permanent-smart-alarm",
+    "features_rich": [
+      {
+        "source": "subscription",
+        "name": "online-backup",
+        "source_value": "free",
+        "expire_date": null
+      },
+      {
+        "source": "campaign",
+        "name": "smart-alarm",
+        "source_value": "permanent-smart-alarm",
+        "expire_date": null
+      }
+    ],
+    "campaigns": [
+      "permanent-smart-alarm"
+    ],
+    "package_id": "premium",
+    "features": [
+      "online-backup",
+        "smart-alarm",
+        "sleep-aid",
+        "analysis",
+        "snore",
+        "sleep-notes",
+        "premium-sounds",
+        "user-sounds",
+        "wake-up-mood",
+        "heart-rate",
+        "weather",
+        "health-kit",
+        "google-fit",
+        "statistics",
+        "philips-hue",
+        "home-kit"
+    ],
+    "trial": null,
+    "expire_date": null
+  }
