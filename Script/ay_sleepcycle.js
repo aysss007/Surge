4b7b969b72b1a13d8f6ec94451b10f0cfbb60c44
aysss <aysss007@users.noreply.github.com>
2020-12-13 10:38:31 +0800

    modify

--- Script/ay_sleepcycle.js
+++ Script/ay_sleepcycle.js
@@ -1,3 +1,123 @@
-let obj = JSON.parse($response.body);
-obj.subscription.package_id= "premium";
-$done({body: JSON.stringify(obj)});
+
+var obj = JSON.parse($response.body);
+
+obj = {
+  "subscriptions": [
+    {
+      "package_id": "free",
+      "features": [
+        "online-backup",
+        "smart-alarm",
+        "sleep-aid",
+        "analysis",
+        "snore",
+        "sleep-notes",
+        "premium-sounds",
+        "user-sounds",
+        "wake-up-mood",
+        "heart-rate",
+        "weather",
+        "health-kit",
+        "google-fit",
+        "statistics",
+        "philips-hue",
+        "home-kit"
+      ]
+    },
+    {
+      "package_id": "premium",
+      "features": [
+        "online-backup",
+        "smart-alarm",
+        "sleep-aid",
+        "analysis",
+        "snore",
+        "sleep-notes",
+        "premium-sounds",
+        "user-sounds",
+        "wake-up-mood",
+        "heart-rate",
+        "weather",
+        "health-kit",
+        "google-fit",
+        "statistics",
+        "philips-hue",
+        "home-kit"
+      ]
+    },
+    {
+      "package_id": "early-adopter",
+      "features": [
+        "smart-alarm",
+        "sleep-aid",
+        "analysis",
+        "snore",
+        "sleep-notes",
+        "premium-sounds",
+        "user-sounds",
+        "wake-up-mood",
+        "heart-rate",
+        "weather",
+        "health-kit",
+        "google-fit",
+        "statistics",
+        "philips-hue",
+        "home-kit"
+      ]
+    },
+    {
+      "package_id": "early-adopter-online-backup",
+      "features": [
+        "online-backup",
+        "smart-alarm",
+        "sleep-aid",
+        "analysis",
+        "snore",
+        "sleep-notes",
+        "premium-sounds",
+        "user-sounds",
+        "wake-up-mood",
+        "heart-rate",
+        "weather",
+        "health-kit",
+        "google-fit",
+        "statistics",
+        "philips-hue",
+        "home-kit"
+      ]
+    },
+    {
+      "package_id": "wake-up",
+      "features": [
+        "online-backup",
+        "smart-alarm",
+        "premium-sounds",
+        "user-sounds",
+        "philips-hue",
+        "home-kit"
+      ]
+    },
+    {
+      "package_id": "fall-asleep",
+      "features": [
+        "online-backup",
+        "sleep-aid"
+      ]
+    },
+    {
+      "package_id": "analyze-sleep",
+      "features": [
+        "online-backup",
+        "analysis",
+        "snore",
+        "sleep-notes",
+        "wake-up-mood",
+        "heart-rate",
+        "weather",
+        "health-kit",
+        "google-fit",
+        "statistics"
+      ]
+    }
+  ]
+}
